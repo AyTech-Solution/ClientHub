5473b99f909f37c4f579caf4593a5f7e87fa37f3
AyTech Solution <teamaytech@gmail.com>
2026-07-02 19:40:07 +0530

    Configure chunk size warning limit for build
    Set chunk size warning limit to 1600 in build config.

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Isse build error bypass ho jayega
+    chunkSizeWarningLimit: 1600,
+  }
 })
